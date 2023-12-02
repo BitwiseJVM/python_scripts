53cdd00e37b825e7c1ad06b2787deaba61766c88
BitwiseJVM <53354314+BitwiseJVM@users.noreply.github.com>
2023-12-02 12:02:17 +0530

    added comments in  minandmax.py

--- minandmax.py
+++ minandmax.py
@@ -1,11 +1,12 @@
 numberList = [15, 85, 35, 89, 125]
-
+#finding a maximum value in the list
 maxNum = numberList[0]
 for num in numberList:
     if maxNum < num:
         maxNum = num
 print(maxNum)
 
+#finding a minimum value in the list
 minNum = numberList[0]
 for num in numberList:
     if maxNum > num:
